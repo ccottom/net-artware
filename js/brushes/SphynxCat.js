352c5b8862e7ff325e3710cb9f64454cdbb7c293
chloe <ccotto@artic.edu>
2021-04-21 10:56:33 -0500

    added brush

---
+++ js/brushes/SphynxCat.js
@@ -0,0 +1,10 @@
+/* global mouseIsPressed, colorMode, RGB, fill, noStroke, ellipse,
+mouseX, mouseY */
+window.brushes.SphynxCat = {
+  name: 'Sphynx Cat',
+  draw: function () {
+    if (mouseIsPressed && !window.dragging) {
+
+    }
+  }
+}
